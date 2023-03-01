6ad7fcf4c2ed75f53c1ed6b92a610e1eec6b5a9f
Amaan Qureshi <amaanq12@gmail.com>
2023-03-01 01:00:08 -0500

    feat: update bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,3 +1,8 @@
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2023 Alex Suraci <suraci.alex@gmail.com>, Amaan Qureshi <amaanq12@gmail.com>,
+// See the LICENSE file in this repo for license details.
+// ------------------------------------------------------------------------------------------------
+
 //! This crate provides Bass language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
@@ -6,7 +11,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_bass::language()).expect("Error loading bass grammar");
+//! parser.set_language(tree_sitter_bass::language()).expect("Error loading Bass grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -28,17 +33,19 @@ pub fn language() -> Language {
     unsafe { tree_sitter_bass() }
 }
 
+/// The source of the Rust tree-sitter grammar description.
+pub const GRAMMAR: &str = include_str!("../../grammar.js");
+
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
+/// The injection query for this language.
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/nvim/injections.scm");
+
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +54,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading Bass language");
+            .expect("Error loading Bass grammar");
     }
 }
