e5c0f2209090b363b2dc220177c73e6adff2ab3c
Alex Suraci <suraci.alex@gmail.com>
2023-04-01 18:55:27 -0400

    highlight Def class in cons, too
    consistent with other builtins, no reason to keep this separate

--- queries/nvim/highlights.scm
+++ queries/nvim/highlights.scm
@@ -33,13 +33,24 @@
 ((symbol) @keyword
   (#any-of? @keyword "do" "doc"))
 
-;; Special Functions
+;; Defining
 
 ((list
   . (symbol) @keyword.function
   . (symbol) @function
   (symbol)? @parameter)
   (#any-of? @keyword.function "def" "defop" "defn"))
+
+((cons
+  . (symbol) @keyword.function
+  . (symbol) @function
+  (symbol)? @parameter)
+  (#any-of? @keyword.function "def" "defop" "defn"))
+
+((symbol) @keyword.function
+  (#any-of? @keyword.function "def" "defop" "defn"))
+
+;; Builtins
 
 ((symbol) @function.builtin
   (#any-of? @function.builtin "dump" "mkfs" "json" "log" "error" "now" "cons" "wrap" "unwrap" "eval" "make-scope" "bind" "meta" "with-meta" "null?" "ignore?" "boolean?" "number?" "string?" "symbol?" "scope?" "sink?" "source?" "list?" "pair?" "applicative?" "operative?" "combiner?" "path?" "empty?" "thunk?" "+" "*" "quot" "-" "max" "min" "=" ">" ">=" "<" "<=" "list->source" "across" "emit" "next" "reduce-kv" "assoc" "symbol->string" "string->symbol" "str" "substring" "trim" "scope->list" "string->fs-path" "string->cmd-path" "string->dir" "subpath" "path-name" "path-stem" "with-image" "with-dir" "with-args" "with-cmd" "with-stdin" "with-env" "with-insecure" "with-label" "with-port" "with-tls" "with-mount" "thunk-cmd" "thunk-args" "resolve" "start" "addr" "wait" "read" "cache-dir" "binds?" "recall-memo" "store-memo" "mask" "list" "list*" "first" "rest" "length" "second" "third" "map" "map-pairs" "foldr" "foldl" "concat" "append" "filter" "conj" "list->scope" "merge" "apply" "id" "always" "vals" "keys" "memo" "succeeds?" "run" "last" "take" "collect" "take-all" "insecure!" "from" "cd" "wrap-cmd" "mkfile" "path-base" "not"))
@@ -100,6 +111,7 @@
 (escape_sequence) @string.escape
 
 (path) @text.uri @string.special
+(command) @string.special
 
 (number) @number
 
